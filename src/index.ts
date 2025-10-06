e4ae16b8ed2e5555de171ec8c6fde93efe2d7dc4
James Benner <hello@jamesbenner.com>
2025-10-06 16:34:59 -0400

    Update logic

--- src/index.ts
+++ src/index.ts
@@ -6,20 +6,11 @@ export async function makeExecutable(filepath: string): Promise<void> {
   let file: FileHandle;
 
   try {
-    file = await open(filepath, 'r');
+    file = await open(filepath);
     const stats = await file.stat();
-    const [owner, group, other] = [...stats.mode.toString(8).slice(-3)].map(Number);
-    const permissions: Record<string, number> = { owner: owner!, group: group!, other: other! };
+    const mode = stats.mode | constants.S_IXUSR | constants.S_IXGRP | constants.S_IXOTH;
 
-    Object.entries(permissions).forEach(([permission, value]) => {
-      if ([0, 4, 6].includes(value)) {
-        permissions[permission]! += 1;
-      }
-    });
-
-    await file.chmod(Number(`0o${permissions.owner}${permissions.group}${permissions.other}`));
-  } catch (error) {
-    console.error(error);
+    await file.chmod(mode);
   } finally {
     // @ts-expect-error File is defined if the try block is executed.
     file?.close();
@@ -27,12 +18,8 @@ export async function makeExecutable(filepath: string): Promise<void> {
 }
 
 export function makeExecutableSync(filepath: string): void {
-  try {
-    const stats = fs.statSync(filepath);
-    const mode = stats.mode | constants.S_IXUSR | constants.S_IXGRP | constants.S_IXOTH;
+  const stats = fs.statSync(filepath);
+  const mode = stats.mode | constants.S_IXUSR | constants.S_IXGRP | constants.S_IXOTH;
 
-    fs.chmodSync(filepath, mode);
-  } catch (error) {
-    console.error(error);
-  }
+  fs.chmodSync(filepath, mode);
 }
